ff36bbe9e2d2b76c34d63206b9f9d38ae56f451d
ashfak99 <aliakbaralam86@gmail.com>
2025-11-05 20:03:05 +0530

    Short Code Form in CP

---
+++ CP/shortCode.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define PB push_back
+
+typedef vector<int> vi;
+
+
+int main(int argc, char const *argv[])
+{
+    vi v;
+    v.PB(1);
+    v.PB(3);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout<<v[i]<< " ";
+    }
+    
+    return 0;
+}
