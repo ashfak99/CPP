42be698019a9050fbe6594435b502c13a5f96691
ashfak99 <aliakbaralam86@gmail.com>
2025-11-07 20:39:52 +0530

    Power Sum and Series

--- CP/extraLongFactorial.cpp
+++ CP/extraLongFactorial.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define FOR(i,n,s) for(int i=s; i<=n; i++)
 #define For(i,s) for(int i=s; i>=0; i--)
 #define PB push_back
+#define GTRIF(x,y) if(x>y)
+#define EQLIF(x,y) if(x==y)
 
 typedef vector<int> vi;
 
@@ -36,8 +38,32 @@ void extraLongFactorial(int n)
     cout<<endl;
 }
 
+int powerSumHelper(int x, int n, int current=1)
+{
+    long long power=1;
+    FOR(i,n,0)
+    {
+        power*=current;
+        GTRIF(power,x) return 0;
+    }
+
+    GTRIF(power,x) return 0;
+    EQLIF(power,x) return 1;
+
+    return powerSumHelper(x-power,n,current+1)+powerSumHelper(x,n,current+1);
+}
+
+int powerSum(int x, int n)
+{
+    int ans=powerSumHelper(x,n,1);
+    return ans;
+}
+
 int main(int argc, char const *argv[])
 {
-    extraLongFactorial(45);
+    //extraLongFactorial(45);
+    int x=sqrt(13);
+    //cout<<x<<endl;
+    cout<<powerSum(100,2);
     return 0;
 }
---
+++ problem_solving/Basics/sseries.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long sequence(long n)
+{
+    return (long long)n*n;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout<<sequence(229137999);
+    return 0;
+}
